1aa106f5b373d9db4562d136291a667f43275730
Fabricio Ronchi <fabricioronchii@gmail.com>
2017-01-10 13:30:48 -0200

    Files

---
+++ www.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express'),
+    path = require('path'),
+    app = express(),
+    logger = require('morgan'),
+    compress = require('compression');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(logger('default'));
+app.use(compress({ level: 9, memLevel: 9 }));
+
+app.get('/', function(req, res) {
+  res.sendfile('views/index.html');
+});
+
+app.listen(2017, function(){
+  console.log('Listening on port 2017!');
+});
